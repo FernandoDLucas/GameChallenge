a1b03e1fd56b74a9a0df84bfcf56cf001a2d7e79
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2021-03-23 09:22:31 -0300

    add images

--- GameChallenge/Helpers/DeckHelper.swift
+++ GameChallenge/Helpers/DeckHelper.swift
@@ -23,46 +23,46 @@ class DeckHelper: SKSpriteNode {
     init(listOfCards: [Card], texture: SKTexture?, superView: SKView) {
         self.listOfCards = listOfCards
         self.superView = superView
-    
+
         let size = CGSize(width: 40, height: 52)
         super.init(texture: texture, color: .blue, size: size)
         self.position = CGPoint(x: superView.frame.size.width - 48, y: superView.frame.size.height - 107)
+        //inclinação da carta de acordo com números ímpares e pares, embora a posiçao seja a mesma
         
+        addCardsInScene()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         popCard()
     }
     
-//    func move(object: SKSpriteNode) {
-//        SKAction.move(to: CGPoint(x: , y: ),
-//                                       duration: TimeInterval())
-//        object.run(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
-//
-//    }
+    func addCardsInScene() {
+        for card in listOfCards {
+            card.flip()
+           // card.zRotation = CGFloat(1)
+            addChild(card)
+        }
+    }
     
     //Escolher uma carta aleatória e entregar ao DisplayCardHelper
     func popCard() {
-        //add textura frente e verso na ultima
         if !listOfCards.isEmpty {
             let card = listOfCards.removeLast()
             card.flip()
-            addChild(card)
-            card.flip()
-            // animacao
             
             if let scene = scene as? GameScene {
-                card.removeFromParent()
+                let move = SKAction.move(to: CGPoint(x: superView.frame.width/2, y: scene.displayCard!.position.y/2), duration: 2)
+                let moveDone = SKAction.removeFromParent()
+                card.run(SKAction.sequence([move, moveDone]))
                 scene.displayCard?.addCard(card: card)
             }
         }
-        
     }
-    
     
     //Mudar a visualização de acordo com a quantidade de cartas
     func changeViewDeck() {
         if listOfCards.isEmpty {
+            //self.texture = SKTexture(imageNamed: "")
             self.color = .white
         } else if listOfCards.count == 15 {
             self.color = .green
