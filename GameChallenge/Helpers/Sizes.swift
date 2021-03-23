4e510072da7eebc74e6887ed4cedfe2a86fc6527
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-03-23 11:59:01 -0300

    sizes to cards

---
+++ GameChallenge/Helpers/Sizes.swift
@@ -0,0 +1,11 @@
+//
+//  Sizes.swift
+//  GameChallenge
+//
+//  Created by Lidiane Gomes Barbosa on 23/03/21.
+//
+
+import UIKit
+let CARD_HAND_SIZE = CGSize(width: 102, height: 141)
+let CARD_DECK_SIZE = CGSize(width: 40, height: 52)
+let DECK_SIZE = CGSize(width: 40, height: 52)
