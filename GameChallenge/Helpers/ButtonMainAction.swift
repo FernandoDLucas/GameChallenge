810a69f829c32c24dd27afcea422d1868eb67ab2
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-04-08 11:25:33 -0300

    update label actual Player

--- GameChallenge/Helpers/ButtonMainAction.swift
+++ GameChallenge/Helpers/ButtonMainAction.swift
@@ -3,32 +3,32 @@
 //  GameChallenge
 //
 //  Created by Lidiane Gomes Barbosa on 25/03/21.
-//
+// swiftlint:disable unused_setter_value
 
 import SpriteKit
 
 class ButtonMainAction: SKSpriteNode {
     private var label = SKLabelNode()
     
+    override var isUserInteractionEnabled: Bool {
+        get {
+            return true
+        }
+        set {
+            // ignore
+        }
+    }
+    
     init(superView: SKView) {
         super.init(texture: nil, color: .clear, size: MAIN_BUTTON_SIZE)
         formatLabel(text: "Passar")
         label.position = CGPoint(x: 0, y: 0)
         
-//        self.blendMode = SKBlendMode.alpha
-//        self.colorBlendFactor = 1
-//        self.color = .black
-//        self.alpha = 0.75
-        
-//        self.lightingBitMask = 0b0001
-//        self.shadowCastBitMask = 0b0001
-        
         let shapeNode = SKShapeNode(circleOfRadius: size.height / 2)
         shapeNode.fillColor = .action
         shapeNode.strokeColor = .action
         shapeNode.blendMode = .replace
-        //shapeNode.glowWidth = 1
-       
+
         addChild(shapeNode)
         addChild(label)
         
@@ -50,5 +50,14 @@ class ButtonMainAction: SKSpriteNode {
         label.numberOfLines = 1
         label.verticalAlignmentMode = .center
         label.horizontalAlignmentMode = .center
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if touches.count == 1 {
+            if let parent = parent as? GameScene {
+                parent.gameManagement.changeActualPlayer()
+                parent.updateLabel()
+            }
+        }
     }
 }
