a124a21562e88128e4685b6e7a3e0b032e425c36
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-04-21 11:25:35 -0300

    cardbuilder

--- GameChallenge/Services/CardBuilder.swift
+++ GameChallenge/Services/CardBuilder.swift
@@ -32,11 +32,11 @@ class BuildCards {
     func buildSpells(card: CardStruct) -> SpellCard {
         switch CardType(rawValue: card.CardType) {
         case .heal:
-            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveHeal: card.EffectValue)
+            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveHeal: card.EffectValue, cardName: card.CardName)
         case .damage:
-            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveDamage: card.EffectValue)
+            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveDamage: card.EffectValue, cardName: card.CardName)
         case .none:
-            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveHeal: card.EffectValue) 
+            return SpellCard.init(cost: card.CostValue, passiveDescription: card.Passive, passiveHeal: card.EffectValue, cardName: card.CardName) 
         }
     }
 }
