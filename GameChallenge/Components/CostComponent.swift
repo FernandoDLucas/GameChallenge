f108977520303c5ea5b35ea0108c98541faf753d
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-04-21 09:17:49 -0300

    updated costComponent

--- GameChallenge/Components/CostComponent.swift
+++ GameChallenge/Components/CostComponent.swift
@@ -13,15 +13,17 @@ class CostComponent: SKSpriteNode {
     let labelNode = SKLabelNode(text: "0")
     var costValue: Int = 0 {
         didSet {
-            labelNode.text = String(self.costValue)
+            labelNode.attributedText = NSAttributedString(string: String(self.costValue), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor: UIColor.text])
         }
     }
     
     init() {
         self.costTexture = SKTexture(imageNamed: "costCard")
-        labelNode.fontColor = .red
-        labelNode.fontSize = 8
-        super.init(texture: costTexture, color: .clear, size: COMPONENT_SIZE)
+        labelNode.position = CGPoint(x: -1, y: -10)
+        super.init(texture: costTexture, color: .clear, size: CGSize(width: 20, height: 25))
+        self.zPosition = Zpositions.cardAttributes.rawValue
+        labelNode.zPosition = Zpositions.cardAttributes.rawValue
+       
         addChild(labelNode)
     }
     
